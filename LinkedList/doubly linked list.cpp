c9388f55baf25a93539932494a042b2e5ea3b4d4
ShyamB123 <shyamer3@gmail.com>
2022-06-13 12:09:34 +0530

    Create doubly linked list.cpp

---
+++ LinkedList/doubly linked list.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+using namespace std;
+
+struct node
+{
+    int data;
+    node * prev;
+    node* next;
+};
+
+void InsertAtHead(int x);
+void InsertAtTail(int x);
+void print();
+void RevPrint();
+
+node* head;
+
+int main()
+{
+    head=NULL;
+
+    InsertAtHead(5); //5
+    InsertAtHead(8); //8 5
+    InsertAtTail(3); //8 5 3
+    InsertAtTail(1); //8 5 3 1
+    print();
+    RevPrint();
+}
+
+void InsertAtHead(int x)
+{
+    node * temp = new node();
+
+    if(head!=NULL)head->prev=temp;
+    temp->data=x;
+    temp->prev=NULL;
+    temp->next= head;
+
+    head=temp;
+}
+
+void InsertAtTail(int x)
+{
+    node*temp=new node();
+    temp->data=x;
+    temp->next=NULL;
+    node* srch=head;
+
+    while(srch->next!=NULL){
+        srch=srch->next;
+
+    }
+    srch->next=temp;
+    temp->prev=srch;
+}
+
+void print()
+{
+    node*temp=head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+
+    }
+    cout<<"\n";
+}
+void RevPrint()
+{
+    node * temp=head;
+    while(temp->next!=NULL)
+    {
+        temp=temp->next;
+    }
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->prev;
+    }
+    cout<<"\n";
+}
