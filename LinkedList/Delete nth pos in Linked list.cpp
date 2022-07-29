a2fb84b41d060586fb681d12a425dc159ecd04f9
ShyamB123 <shyamer3@gmail.com>
2022-07-29 18:27:13 +0530

    Update Delete nth pos in Linked list.cpp

--- LinkedList/Delete nth pos in Linked list.cpp
+++ LinkedList/Delete nth pos in Linked list.cpp
@@ -53,3 +53,20 @@ void Insert(int x)
     temp2->next =temp1;
 
 }
+void Delete(int n)
+{
+    struct node* temp1 = head;
+    if(n==1){
+        head = temp1->next;
+        free(temp1);
+        return;
+    }
+    int i;
+    for(i=1;i<n-1;i++)
+    {
+        temp1 = temp1->next;
+    }
+    struct node* temp2 = temp1->next;
+    temp1->next = temp2->next;
+    free(temp2);
+}
