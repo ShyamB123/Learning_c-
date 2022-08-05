6dfe94cad7c8764344b6407b028f3e13c7898350
ShyamB123 <shyamer3@gmail.com>
2022-08-05 22:28:49 +0530

    Update odd_FindIth_SetIth.cpp

--- Bit Manipulation/odd_FindIth_SetIth.cpp
+++ Bit Manipulation/odd_FindIth_SetIth.cpp
@@ -4,20 +4,21 @@
 
  bool isOdd (int n)
  {
-     return (n&1);
+     return (n&1); // if last bit is 1 it return 1 which is taken as boolean true
+                    // if last bit is 0 it returns 0 which is taken as boolean false
  }
 
  bool checkith(int n,int i)
  {
-     int mask = 1<<i;
-     return (n&mask);
+     int mask = 1<<i;           //moves 1 to ith position
+     return (n&mask);           // returns value of the ith bit
 
  }
 
  void SetIth(int& n,int i)
  {
-     int mask = 1<<i;
-     n = (n|mask);
+     int mask = 1<<i;           //moves 1 to the ith bit
+     n = (n|mask);              // sets ith bit to 1 regardless of the value at ith bit using OR
  }
  int main()
  {
