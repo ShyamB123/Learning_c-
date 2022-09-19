2c695b31ad8f125c4fbd4ed68901d54e555ebc4e
ShyamB123 <shyamer3@gmail.com>
2022-09-19 10:12:25 +0530

    Create Ascii_Subsequences.cpp

---
+++ recursion/Problems/Ascii_Subsequences.cpp
@@ -0,0 +1,51 @@
+ #include<iostream>
+#include<cstring>
+#include<bits/stdc++.h>
+ using namespace std;
+
+
+ int k= 0 ;
+
+ void subseq(string a , string out , int i )
+ {
+     if(a[i] ==  '\0')
+     {
+        // out = out + "\0";
+        
+         cout << out << " ";
+         k++;
+
+         return;
+     }
+
+        subseq(a,out,i+1);
+        //out[j] = a[i];
+        
+        subseq(a,out + a[i],i+1);
+
+        //out.erase(i);
+      //  out  =  out + to_string(int(a[i]));
+         subseq(a,out + to_string(int(a[i])),i+1);
+
+ }
+
+ int main()
+ {
+     
+         string a ;
+         cin>>a;
+
+
+        string out ;
+     
+
+     subseq(a,out,0);
+
+    
+
+    
+     cout << endl;
+     cout << k <<endl;
+
+ 
+ }
