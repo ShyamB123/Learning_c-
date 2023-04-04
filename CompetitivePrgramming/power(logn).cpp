e1703dd9db11e6fa30215f3ec66c263d9d5ddb35
ShyamB123 <shyamer3@gmail.com>
2023-04-04 11:11:11 +0530

    updating cp folder

--- CompetitivePrgramming/power(logn).cpp
+++ CompetitivePrgramming/power(logn).cpp
@@ -9,13 +9,4 @@
 
 using namespace std;
 
-long long binpow(long long a, long long b) {
-    long long res = 1;
-    while (b > 0) {
-        if (b & 1)
-            res = res * a;
-        a = a * a;
-        b >>= 1;
-    }
-    return res;
-}
+c
